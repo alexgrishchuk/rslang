6de8fdf53be98c78474c226b80e86f8c6df33613
Sergey <barclays1313@gmail.com>
2022-02-19 23:43:42 +0300

    feat: implement main page

---
+++ src/modules/main/components/start-learning.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+function StartLearning() {
+  const steps = ['Найти наше приложение', 'Зарегистрироваться', 'Начать обучение и отслеживать свои успехи!'];
+
+  return (
+    <Box sx={{ width: '100%' }} m={6}>
+      <Typography variant="h4" gutterBottom component="div" align="center" mb={4}>
+        Как начать обучение?
+      </Typography>
+      <Stepper activeStep={1} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
+
+export default StartLearning;
