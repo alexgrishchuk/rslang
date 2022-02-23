2079780f10b21272b91ca7892766e4fb7b4c25eb
Alex Grishchuk <alex_grishchuk@tut.by>
2022-02-23 21:09:57 +0300

    feat: add request

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-    extends: ["airbnb-typescript-prettier"],
-  };
+  extends: ["airbnb-typescript-prettier"],
+  rules: {
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }],
+  },
+};
--- src/backend-requests/aggregated-words-requests.ts
+++ src/backend-requests/aggregated-words-requests.ts
@@ -1,16 +1,23 @@
 import { getUserIdFromStorage } from '../storage/storage';
 import { request } from './utils/common-request';
 import ENDPOINTS from './data/endpoints';
-import { WordInfo } from './words-requests';
+import { WordInfo, getWords } from './words-requests';
+import config from '../config.json';
 
-const WORDS_PER_PAGE_DEFAULT = 20;
+const WORDS_PER_PAGE_DEFAULT = +config.wordsPerPageDefault;
+const PAGES_COUNT = +config.pagesCount;
+const WORDS_PER_GROUP = WORDS_PER_PAGE_DEFAULT * PAGES_COUNT;
+
+interface ModifiedWordInfo extends Omit<WordInfo, 'id'> {
+  _id: string;
+}
 
 interface ICount {
   count: number;
 }
 
 export interface IAgrWords {
-  paginatedResults: Array<WordInfo>;
+  paginatedResults: Array<ModifiedWordInfo>;
   totalCount: Array<ICount>;
 }
 
@@ -33,7 +40,12 @@ async function getUserAggregatedWords(
   return response && response.ok ? response.json() : null;
 }
 
-export async function getCurrentUserAggregatedWords(group: number, page: number, wordsPerPage: number, filter: string) {
+async function getCurrentUserAggregatedWords(
+  group: number,
+  page: number,
+  wordsPerPage: number,
+  filter: string
+): Promise<IAgrWords[] | null> {
   const userId = getUserIdFromStorage();
   return userId !== null ? getUserAggregatedWords(userId, group, page, wordsPerPage, filter) : null;
 }
@@ -51,4 +63,62 @@ export async function isAllWordsOnPageLearned(
   );
 
   return !!wordsArray && !!wordsArray[0].totalCount.length && wordsArray[0].totalCount[0].count >= wordsPerPage;
+}
+
+async function getUserUserWordsByGroup(
+  userId: string,
+  group: number,
+  wordsPerPage: number,
+  filter: string
+): Promise<IAgrWords[] | null> {
+  const init: RequestInit = {
+    method: 'GET',
+  };
+
+  const requestInfo = `${ENDPOINTS.getAggregatedWords(
+    userId
+  )}?group=${group}&wordsPerPage=${wordsPerPage}&filter=${filter}`;
+  const response = await request(requestInfo, init);
+
+  return response && response.ok ? response.json() : null;
+}
+
+async function getCurrentUserWordsByGroup(
+  group: number,
+  wordsPerPage: number,
+  filter: string
+): Promise<IAgrWords[] | null> {
+  const userId = getUserIdFromStorage();
+  return userId !== null ? getUserUserWordsByGroup(userId, group, wordsPerPage, filter) : null;
+}
+
+export async function getNotLearnedWords(group: number, page: number, wordsCount: number): Promise<WordInfo[]> {
+  const learnedWordsArray = await getCurrentUserWordsByGroup(
+    group,
+    WORDS_PER_GROUP,
+    `{"$and":[{"userWord.optional.isLearned":true}, {"group":${group}}]}`
+  );
+
+  const promises: Promise<WordInfo[]>[] = [];
+  for (let i = 0; i <= page; i += 1) {
+    promises.push(getWords(group, i));
+  }
+
+  const allWordsToPage = (await Promise.all(promises)).reduce((prev, item) => prev.concat(item), []);
+
+  const learnedWordsIdArray = learnedWordsArray ? learnedWordsArray[0].paginatedResults.map((word) => word._id) : [];
+  const learnedWordsSet = new Set(learnedWordsIdArray);
+
+  const resultArray: WordInfo[] = [];
+  let currentPage = page;
+  const filterFunction = (word: WordInfo) => word.page === currentPage && !learnedWordsSet.has(word.id);
+
+  do {
+    const notLearnedArray = allWordsToPage.filter(filterFunction);
+    const addSize = wordsCount - resultArray.length;
+    resultArray.push(...notLearnedArray.slice(0, addSize));
+    currentPage -= 1;
+  } while (currentPage >= 0 && resultArray.length < wordsCount);
+
+  return resultArray;
 }
